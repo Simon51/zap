9a0893de6cbc1884b7945d7950ac9d89a563b7e2
TheBirdyDeveloper <tehbirdy3@gmail.com>
2016-08-30 10:13:30 +0200

    Ecriture des commentaires

--- js/FirstTry_Angular.js
+++ js/FirstTry_Angular.js
@@ -7,34 +7,30 @@ var app = angular.module('app',['nvd3','ui.bootstrap']);
 app.controller("ReportCtrl",function($scope, $http, $rootScope, $sce) {
 	$scope.isArray = angular.isArray;
 	$scope.isCollapsed = true;
-	$http.get("res/grosZap1.xml", {
-//ReportForSpiderWithActiveScan-17-11-07.xml
-
+	$http.get("res/grosZap.xml", {
 		transformResponse:function(data) {
 			                      // convert the data to JSON and provide
 			                      // it to the success function below
 			                        var x2js = new X2JS();
 			                        var json = x2js.xml_str2json( data );
-			//console.log(json);
 			                        var report = $scope.traitementReport(json);
 									return report;
-
 			               }
 	                 
 	})
 	.then(function(response) {
+		
+		//Variables 
 		$scope.content = response.data;
 		$scope.values = $scope.Risk($scope.content);
 		$scope.nbvuln = $scope.NbRisk($scope.content);
-		console.log($scope.values);
-		console.log($scope.content);
-		
 		$scope.low = Math.round((($scope.nbvuln[0]/($scope.nbvuln[1]+$scope.nbvuln[0]+$scope.nbvuln[2]))*100));
 		$scope.nbLow=$scope.nbvuln[0];
 		$scope.med = Math.round((($scope.nbvuln[1]/($scope.nbvuln[1]+$scope.nbvuln[0]+$scope.nbvuln[2]))*100));
 		$scope.nbMed=$scope.nbvuln[1];
 		$scope.high = Math.round((($scope.nbvuln[2]/($scope.nbvuln[1]+$scope.nbvuln[0]+$scope.nbvuln[2]))*100));
 		$scope.nbHigh=$scope.nbvuln[2];
+		
 		if ($scope.high === 0 && $scope.nbHigh !== 0) {
 			$scope.high = '1';
 		}
@@ -45,6 +41,7 @@ app.controller("ReportCtrl",function($scope, $http, $rootScope, $sce) {
 			$scope.low = '1';
 		}
 		
+		//Variable pour remplir le graphe
 		$scope.data = [
 		               {
 		            	   key: "Low: " + $scope.nbvuln[0],
@@ -58,22 +55,16 @@ app.controller("ReportCtrl",function($scope, $http, $rootScope, $sce) {
 		            	   key: "High: " + $scope.nbvuln[2] ,
 		            	   y: $scope.high*2
 		               }
-		               ];
-		
-		
-			
+		               ];	
 	});
 
 
+	//Methode pour determiner le nombre d'alertes par type
 	$scope.NbRisk = function(contenu) {
-		//console.log('contenu !! ' + JSON.stringify(contenu));
 		var compteur = [0,0,0];
 		angular.forEach(contenu, function(site, key) {
-			//console.log('contenu ZZ!! ');
 			angular.forEach(site.alerts,function(alert, key) {
-				//console.log('alerts.alertitem.riskcode : ' + JSON.stringify(alert));
 				angular.forEach(alert,function(item, key) {
-					//console.log('item.count');
 					if (item.riskcode) {
 						compteur[item.riskcode-1] = compteur[item.riskcode-1] + parseInt(item.count);
 					}
@@ -83,14 +74,11 @@ app.controller("ReportCtrl",function($scope, $http, $rootScope, $sce) {
 		return compteur;
 	};
 	
-	
+	//Methode pour determiner la quantite pour chaque type d'alerte
 	$scope.Risk = function(contenu) {
-		console.log('contenu !! ' + JSON.stringify(contenu));
 		var compteur = [0,0,0];
 		angular.forEach(contenu, function(site, key) {
-			console.log('contenu ZZ!! ');
 			angular.forEach(site.alerts,function(alert, key) {
-				console.log('alerts.alertitem.riskcode : ' + JSON.stringify(alert));
 				angular.forEach(alert,function(item, key) {
 					if (item.riskcode) {
 						compteur[item.riskcode-1] = compteur[item.riskcode-1] + 1;
@@ -102,16 +90,15 @@ app.controller("ReportCtrl",function($scope, $http, $rootScope, $sce) {
 	}; 
 
 
+	//Methode permettant de supprimer les sites ayant des alerts vides. 
 	$scope.traitementReport = function (report) {
 		var newReport = []
 		console.log(JSON.stringify(report));
-		
 		if(!angular.isArray(report.OWASPZAPReport.site)){
 			var site = report.OWASPZAPReport.site;
 			report.OWASPZAPReport.site=[];
 			report.OWASPZAPReport.site.push(site);
 		}
-		
 		angular.forEach(report.OWASPZAPReport.site,function(site, key){
 			if (site.alerts!==""){
 				if (!angular.isArray(site.alerts.alertitem)){
@@ -119,15 +106,16 @@ app.controller("ReportCtrl",function($scope, $http, $rootScope, $sce) {
 					site.alerts.alertitem = [];
 					site.alerts.alertitem.push(tmp);
 				}
-
 				newReport.push(site);
 			}
 		});
 		return newReport;
 	};  
 
+	//Methode permettant d'interpreter du langage HTML
 	$scope.SkipValidation = function(value) { return $sce.trustAsHtml(value); }; 
 
+	//Variable qui definit les options du graphe
 	$scope.options = {
 			chart: {
 
@@ -161,12 +149,7 @@ app.controller("ReportCtrl",function($scope, $http, $rootScope, $sce) {
 				tooltip: {
 					enabled : false
 				}
-
 			}
-
 	};
-
-
-
 });
 
